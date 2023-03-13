d0f686aad7dc72eeb9e5c9db28099fa83f6df685
Gustav Halvorsen <gustavh@student.uia.no>
2023-03-13 18:52:59 +0100

    Reset App.js and moved the code to new JS file inside components folder

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-import {useEffect, useState} from "react";
-import {doc, deleteDoc } from "firebase/firestore";
-import {db} from "./firebaseConfig";
-
-const deleteGathering = async (docName) => {
-  const colName = "gathering";
-  const docRef = doc(db, colName, docName);
-
-  await deleteDoc(docRef);
-}
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [buttonText, setButtonText] = useState("");
-  const [gatheringDel, setGatheringDel] = useState("");
-
-  useEffect(() => {
-    deleteGathering(gatheringDel)
-        .then(() => {
-          console.log("Removal successful!");
-        })
-        .catch(() => {
-          console.log("No such document!");
-        })
-  }, [gatheringDel])
-
-  return (
-    <View style={styles.container}>
-      <TextInput onChangeText={setButtonText} placeholder={"Gathering"} />
-      <Button title="Press me!" onPress={() => setGatheringDel(buttonText.toLowerCase())}/>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });

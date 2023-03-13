bd34f14cd36c6e1404d08336a2a8c19a742a0609
Ammar Ali <ammara@student.uia.no>
2023-03-13 19:18:49 +0100

    Merged login with create gathering. Login works but im trying to create so that when u login, thats when u get to create a gathering. Login works but its not displaying atm

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StyleSheet} from 'react-native';
 import {login} from "./components/loginView";
+import { StatusBar } from 'expo-status-bar';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Create from "./components/CreateGathering";
 
 
 
-export default function App() {
-  return (login());
+const App = () => {
+
+
+
+  //The navigation bar that you see at the bottom
+  const Tab = createBottomTabNavigator();
+  login();
+
+  return (
+
+
+
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Create" component={Create}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+
+
+);
+
+
+
 }
 
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ components/CreateGathering.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View} from "react-native";
+import {doc, getDoc, setDoc} from "firebase/firestore";
+import {db} from "../firebaseConfig";
+
+
+
+const Create = () => {
+
+
+
+
+    function GenerateID() {
+        const number1 = Math.floor(Math.random() * 9) + 1;
+        const number2 = Math.floor(Math.random() * 9) + 1;
+        const number3 = Math.floor(Math.random() * 9) + 1;
+        const number4 = Math.floor(Math.random() * 9) + 1;
+        const number5 = Math.floor(Math.random() * 9) + 1;
+
+        return 'gathering' + number1 + number2 + number3 + number4 + number5;
+    }
+
+
+    async function add(id, gathName, gathTime, gathDate) {
+        await setDoc(doc(db, "gathering", id), {
+                name: gathName,
+                time: gathTime,
+                date: gathDate,
+            }
+        );
+    }
+
+
+    async function check(collection, id) {
+        const docRef = doc(db, collection, id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            check("gathering", GenerateID()).then();
+        } else {
+            add(id, gathName, gathTime, gathDate).then();
+        }
+    }
+
+
+    const [gathName, set_gathName] = useState("Name");
+    const [gathTime, set_gathTime] = useState("time");
+    const [gathDate, set_gathDate] = useState("mm.dd.yyyy");
+
+    const addData = () => {
+        check("gathering", GenerateID()).then();
+    }
+
+
+
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+            <Text>Gathering name:</Text>
+            <TextInput
+                onChangeText={set_gathName}
+                placeholder="gathering name"
+            />
+
+            <Text>Time</Text>
+            <TextInput
+                onChangeText={set_gathTime}
+                placeholder="Time"
+            />
+
+            <Text>Date</Text>
+            <TextInput
+                onChangeText={set_gathDate}
+                placeholder="Date"
+            />
+
+            <Button
+                onPress={addData}
+                title="Add"
+            />
+        </View>
+    );
+
+}
+
+
+export default Create;
+
+

0c28d4fde3f1fe39f14577f7dc18f60554b03b95
Kristian Fredriksen Enger <kristianfe@student.uia.no>
2023-03-14 13:28:02 +0100

    endret slik at edit funker men homescreen ser ikke pen ut

--- components/EditGathering.js
+++ components/EditGathering.js
@@ -2,11 +2,11 @@
 import {doc, setDoc} from "firebase/firestore";
 import {db} from "../firebaseConfig";
 
-export async function edit(id, Name, Date, Time) {
+export async function edit(id, gatName, gatDate, gatTime) {
     await setDoc(doc(db, "gathering", id), {
-            name: Name,
-            date: Date,
-            time: Time
+            name: gatName,
+            date: gatDate,
+            time: gatTime
         }
     );
 

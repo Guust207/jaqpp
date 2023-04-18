3f79670be31477faa33cbcbd6b879c3d6fdea48e
Kissi <50911745+kfenger@users.noreply.github.com>
2023-04-18 10:35:53 +0200

    fikset sp man kan edit gathering og delete gatering i gatering view

--- components/gatheringComponents/GatheringView.js
+++ components/gatheringComponents/GatheringView.js
@@ -1,10 +1,13 @@
-import {Button, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import React from "react";
+import {Button, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, {useState} from "react";
 import {currentGathering} from "../global_variables";
 import {Gathering} from "./Gathering";
 import { useNavigation } from '@react-navigation/native';
 
 import Edit from "./EditGathering";
+import {Modal} from "../Modal";
+import {deleteDoc, doc, getDoc, setDoc} from "firebase/firestore";
+import {auth, db} from "../../firebaseConfig";
 
 
 /*
@@ -12,38 +15,183 @@ OBS!!!!!!
 Edit og Delete skal være tilgjengelig når du klikker på en av gatheringene
  */
 export const GatheringView = ({route}) => {
+    const navigation = useNavigation();
+
+
     const { item } = route.params;
+    const user = auth.currentUser;
+
+    //Edit part
+    //Variables and functions that handles the edit part
+    const [id, set_id] = useState(item.id);
+    const [Name, set_Name] = useState(Name);
+    const [Date, set_Date] = useState(Date);
+    const [Time, set_Time] = useState(Time);
+
+
+
+    //Function for editing a gathering
+    async function edit(id, gatName, gatDate, gatTime) {
+        await setDoc(doc(db, "gathering", id), {
+                name: gatName,
+                date: gatDate,
+                time: gatTime,
+                userID: user.uid,
+            }
+        );
+    }
+
+
+
+    //This is  the Edit function for the Edit button located at the Home in the app
+    //Its main responsibility is getting the documentID of the column selected, then fetching the edited variables.
+    const EditBtnFunc = (gat) => {
+        set_id(gat.id);
+        set_Name(gat.name);
+        set_Date(gat.date);
+        set_Time(gat.time);
+        handleModal();
+    }
+
+    async function check(collection, id) {
+        const docRef = doc(db, collection, id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            edit(id, Name, Date, Time).then();
+            handleModal();
+        }
+    }
+
+
+    //Using this will check the id and then edit the data.
+    const editData = () => {
+        check("gathering", id).then();
+    }
+
+    //Variables used to handle Modal (Popup screen for edit)
+    const [isModalVisible, setIsModalVisible] = React.useState(false);
+    const handleModal = () => {
+        setIsModalVisible(() => !isModalVisible)
+
+    };
+
+
+
+    //Delete part
+    async function checkDel(collection, id) {
+        const docRef = doc(db, collection, id);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            del("gathering", id).then();
+            navigation.navigate('YourGatherings')
+            console.log('DELETED the document');
+        }
+    }
+    async function del(collection, id) {
+        await deleteDoc(doc(db, collection, id));
+    }
+
+    const deleteData = (gat) => {
+        checkDel("gathering", id).then();
+    }
+
+
+
 
     const [CurrentGathering, setCurrentGathering] = currentGathering();
     return (
-        <View>
-            <Text>
-                Hello {item.name}
-            </Text>
-            <Button
-                title={"Administer Budget"}
-            />
-            <Button
-                title={"Administer Attendees"}
-            />
-            <Button
-                title={"Delete Gathering"}
-            />
-            <Button
-                title={"Edit Gathering"}
-            />
-        </View>
+        <View style={styles.container}>
+            <ScrollView>
+                <View>
+                    <Text>
+                        Hello {item.name}
+                    </Text>
+                    <Button
+                        title={"Administer Budget"}
+                    />
+                    <Button
+                        title={"Administer Attendees"}
+                    />
+                    <Button
+                        title={"Delete Gathering"}
+                    />
+                    <Button
+                        title={"Edit Gathering"}
+                    />
+                    <View style={styles.gat}>
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity onPress={() => EditBtnFunc(item)}>
+                                <Text style={styles.button}> Edit  </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => deleteData(item)}>
+                                <Text style={styles.button}>Delete</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                    {/* This is the popup that appears when you click edit at the table */}
+                    <Modal isVisible={isModalVisible}>
+                        <Modal.Container style={styles.modalContainer}>
+                            <Modal.Header title="Edit"/>
+                            <Modal.Body>
+                                <View style={styles.container}>
+                                    <Text style={styles.textModal}>New name for gathering:</Text>
+                                    <View style={styles.inputContainer}>
+                                        <TextInput
+                                            style={styles.input}
+                                            onChangeText={set_Name}
+                                            value={Name}
+                                            placeholder='Name'
+                                        />
+                                    </View>
+                                    <Text style={styles.textModal}>Date:</Text>
+                                    <View style={styles.inputContainer}>
+                                        <TextInput
+                                            style={styles.input}
+                                            onChangeText={set_Date}
+                                            value={Date}
+                                            placeholder='Date'
+                                        />
+                                    </View>
+                                    <Text style={styles.textModal}>Time:</Text>
+                                    <View style={styles.inputContainer}>
+                                        <TextInput
+                                            style={styles.input}
+                                            onChangeText={set_Time}
+                                            value={Time}
+                                            placeholder='Time'
+                                        />
+                                    </View>
+                                </View>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <View style={styles.buttonContainer}>
+                                    <TouchableOpacity onPress={() => editData()}>
+                                        <Text style={styles.editButton}> Edit  </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => handleModal()}>
+                                        <Text style={styles.cancelButton}>Cancel</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </Modal.Footer>
+                        </Modal.Container>
+                    </Modal>
+                </View>
+        </ScrollView>
+    </View>
 
     )
 }
 
 const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            padding: 5,
-            justifyContent: 'flex-start',
-            backgroundColor: 'white'
-        },
+    container: {
+        flex: 1,
+        padding: 5,
+        justifyContent: 'flex-start',
+        backgroundColor: '#D6D5C9',
+    },
         head: {
             height: 44,
             backgroundColor: 'gray'
@@ -53,12 +201,6 @@ const styles = StyleSheet.create({
             fontWeight: 'bold',
             textAlign: 'center',
             color: 'black'},
-        text: {
-            margin: 4,
-            fontSize: 16,
-            textAlign: 'center',
-            backgroundColor: 'blue'
-        },
         category: {
             backgroundColor: 'lightgrey',
             padding: 20
@@ -70,25 +212,86 @@ const styles = StyleSheet.create({
             backgroundColor: 'grey',
             textAlign: 'center'
         },
-        profilePicture: {
-            width: 50,
-            height: 50,
-            borderRadius: 25,
-            marginRight: 10,
-        },
-        name: {
-            fontSize: 18,
-            fontWeight: 'bold',
-        },
-        bioContainer: {
-            marginBottom: 20,
-        },
-        bio: {
-            fontSize: 16,
-            lineHeight: 24,
-        },
-    }
-)
+
+
+    gatContainer: {
+        backgroundColor: '#B9BAA3',
+        padding: 10,
+        margin:10,
+        borderRadius: '10%',
+    },
+    gat: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+    },
+
+    text: {
+        fontSize: 16,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    button: {
+        backgroundColor: '#0A100D',
+        color: '#B9BAA3',
+        borderRadius: 5,
+        paddingVertical: 3,
+        paddingHorizontal: 57,
+        flex: 1,
+        marginHorizontal: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlignVertical: 'center',
+    },
+
+
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        margin: 20,
+    },
+
+    textModal: {
+        fontSize:16,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 2,
+    },
+    input: {
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: '#666',
+        color: '#333',
+        fontSize: 16,
+        marginBottom: 30,
+
+    },
+    editButton: {
+        color: '#005cfc',
+        fontSize: 20,
+        marginRight: 20,
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 5,
+    },
+    cancelButton: {
+        color: '#FF0400',
+        fontSize: 20,
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 5,
+    },
+
+});
+
 
 
 

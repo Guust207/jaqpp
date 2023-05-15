61b7463ee10ed20cbbc5428e74144eca320d787c
Ammar Ali <ammara@student.uia.no>
2023-05-15 14:47:06 +0100

    Can add images when creating gathering
    a depedency/pacakge was added called: expo-image-picker

--- components/gatheringComponents/CreateGathering.js
+++ components/gatheringComponents/CreateGathering.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
-import {TouchableOpacity, Text, TextInput, View} from "react-native";
+import {TouchableOpacity, Text, TextInput, View, Button, Image, ActivityIndicator} from "react-native";
 import {auth, db} from "../../firebaseConfig";
 import {collection, doc, getDoc, setDoc, addDoc} from "firebase/firestore";
 import {currentUser} from "../global_variables";
 import {useNavigation} from "@react-navigation/native";
 import {styles} from "../Styles";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/storage'
+import { initializeApp } from "firebase/app";
+import * as ImagePicker from "expo-image-picker";
 
 const Create = () => {
     const navigation = useNavigation();
@@ -23,9 +27,10 @@ const Create = () => {
     const [gathTime, set_gathTime] = useState("time");
     const [gathDate, set_gathDate] = useState("mm.dd.yyyy");
     const [gathDescription, set_gathDescription] = useState("");
+    const [image, setImage] = useState(null)
+    const [uploading, setUploading] = useState(false)
 
-
-    async function add(id, gathName, gathTime, gathDate, gathDescription) {
+    async function add(id, gathName, gathTime, gathDate, gathDescription, image) {
         const gatheringRef = doc(db, "gathering", id);
 
         // Add gathering data to the main 'gathering' collection
@@ -35,6 +40,7 @@ const Create = () => {
             date: gathDate,
             description: gathDescription,
             userID: user.id,
+            image: image,
         });
 
         const attendeesRef = collection(gatheringRef, 'attendees');
@@ -52,16 +58,69 @@ const Create = () => {
         if (docSnap.exists()) {
             check("gathering", GenerateID()).then();
         } else {
-            add(id, gathName, gathTime, gathDate, gathDescription).then();
+            add(id, gathName, gathTime, gathDate, gathDescription, image).then();
         }
         navigation.navigate('Your gatherings');
     }
 
 
-   const addData = () => {
+    const addData = () => {
         check("gathering", GenerateID()).then();
     }
 
+
+
+    const uploadImage = async () => {
+        const blob = await new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function() {
+                resolve(xhr.response);
+            };
+            xhr.onerror = function() {
+                console.log("failed");
+            };
+            xhr.responseType = 'blob';
+            xhr.open('GET', ImagePicker, true);
+            xhr.send(null);
+        })
+        const ref = firebase.storage().ref().child(`Pictures/Image1`)
+        const snapshot = ref.put(blob)
+        snapshot.on(firebase.storage.TaskEvent.STATE_CHANGED,
+
+            ()=>{
+                setUploading(true)
+            },
+
+            (error) => {
+                setUploading(false)
+                blob.close()
+            },
+            () => {
+                snapshot.snapshot.ref.getDownloadURL().then((url) => {
+                    setUploading(false)
+                    setImage(url)
+                    blob.close()
+                    return url
+                })
+            }
+        )
+    }
+
+
+
+
+
+    const selectImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            aspect: [4,3],
+            quality: 1,
+        });
+        console.log(result);
+        if (!result.canceled && result.assets && result.assets.length > 0) {
+            setImage(result.assets[0].uri);
+        }
+    };
 
 
     return (
@@ -99,7 +158,13 @@ const Create = () => {
                     placeholderTextColor="#999"
                 />
             </View>
+            <View style={styles.inputContainer}>
+                {image && <Image source={{uri: image}} style={{width: 170 , height: 170}}/>}
+                <Button title='Select Image' onPress={selectImage} />
+                {!uploading ? <Button title='Upload Image'        onChangeText={setImage}
+                                      onPress={uploadImage} />: <ActivityIndicator size={'small'} color='black' />}
 
+            </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.createButton} onPress={() => addData()}>
                     <Text style={styles.buttonText}>Create</Text>
